0ba5669be487bf3a0d468daa8c84608803bec2ff
GabelHub <m_gabel@gmx.de>
2019-08-12 17:06:53 +0200

    Included all functions in one file

---
+++ famospy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='famospy',
+      version='0.0',
+      description='A flexible algorithm for model selection',
+      url='http://github.com/GabelHub/FAMoSPy',
+      author='Michael Gabel',
+      author_email='m_gabel@gmx.de',
+      license='MIT',
+      packages=['famospy'],
+      zip_safe=False)
